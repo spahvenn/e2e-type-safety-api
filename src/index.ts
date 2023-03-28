764b4027cd569bd21687b1cf4cfdd47f3beeb0d8
Sami Ahvenniemi <spahvenn@gmail.com>
2023-03-28 11:11:21 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// src/index.ts
+
+// 1
+import { createYoga } from "graphql-yoga";
+import { createServer } from "node:http";
+import { schema } from "./schema";
+
+const port = Number(process.env.API_PORT) || 4000
+
+const yoga = createYoga({schema})
+const server = createServer(yoga);
+server.listen(port, () => {
+  console.log(`🚀 GraphQL Server ready at http://localhost:${port}/graphql`);
+});
